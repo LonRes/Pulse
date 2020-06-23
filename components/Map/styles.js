36144b83d3d39608da31df62886d84cb5fd0a2d6
Simon Dell <simon.dell@founders4schools.org.uk>
2020-06-23 23:38:57 +0100

    Add a flash to the map pins
    Uses the emotion keyframes tool and css object notation to apply an animated background colour to the green dots.

--- components/Map/styles.js
+++ components/Map/styles.js
@@ -1,18 +1,29 @@
+import { keyframes } from '@emotion/core'
 import styled from '@emotion/styled';
 
-export const Container = styled.section`
-    position: relative;
-
-    .leaflet-container {
-        height: 100%;
-        width: 100%;
+const announce = keyframes`
+    from {
+        background-color: orange;
     }
 
-    .property {
-        border-radius: 50%;
+    to {
         background-color: green;
     }
-`;
+`
+
+export const Container = styled.section({
+    position: 'relative',
+
+    '& .leaflet-container': {
+        height: '100%',
+        width: '100%',
+    },
+    '& .property': {
+        animation: `${announce} 400ms 4 both linear`,
+        borderRadius: '50%',
+        backgroundColor: 'green',
+    }
+});
 
 export const Button = styled.button`
     position: absolute;
